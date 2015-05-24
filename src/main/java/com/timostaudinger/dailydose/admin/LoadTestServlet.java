2c42d5b3b6398446ad6202bbfbb487489a9836a5
Timo Staudinger <staudinger.timo@gmail.com>
2015-05-23 22:06:04 -0400

    Add test for getDailyTopOf() to LoadTestServlet. Enables remote testing of load mechanism.

--- src/main/java/com/timostaudinger/dailydose/admin/LoadTestServlet.java
+++ src/main/java/com/timostaudinger/dailydose/admin/LoadTestServlet.java
@@ -1,5 +1,6 @@
 package com.timostaudinger.dailydose.admin;
 
+import com.timostaudinger.dailydose.exception.RedditLoadException;
 import com.timostaudinger.dailydose.reddit.Subreddit;
 import net.dean.jraw.models.Submission;
 import net.dean.jraw.paginators.Sorting;
@@ -7,13 +8,14 @@ import net.dean.jraw.paginators.SubredditPaginator;
 import net.dean.jraw.paginators.TimePeriod;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-public class RedditMailerServlet extends HttpServlet {
+@WebServlet(name = "loadtest", urlPatterns = {"/test/load"})
+public class LoadTestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -22,6 +24,15 @@ public class RedditMailerServlet extends HttpServlet {
 
         for (Submission submission : getMotivated.next()) {
             response.getWriter().println(submission.getTitle());
+        }
+
+        response.getWriter().println("\n-----------------\n");
+
+        try {
+            String dailyTop = Subreddit.getDailyTopOf("getmotivated").getTitle();
+            response.getWriter().println(dailyTop);
+        } catch (RedditLoadException e) {
+            response.getWriter().println(e.getMessage());
         }
 
     }
