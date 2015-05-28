7e5a1536055dd23a16d955dc8365858427330952
Timo Staudinger <staudinger.timo@gmail.com>
2015-05-27 22:56:11 -0400

    Move config files to Resources folder. Moved the application's config file from the WEB-INF/classes folder to the Resources folder to clean up the project structure.

--- src/main/java/com/timostaudinger/dailydose/exception/RedditLoadException.java
+++ src/main/java/com/timostaudinger/dailydose/exception/RedditLoadException.java
@@ -1,6 +1,6 @@
 package com.timostaudinger.dailydose.exception;
 
-public class RedditLoadException extends Exception {
+public class RedditLoadException extends RedditException {
     public RedditLoadException(String message) {
         super(message);
     }
