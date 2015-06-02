5857eaaf6cbcb4f0e0f30dae8496926dff4cfc07
Timo Staudinger <staudinger.timo@gmail.com>
2015-06-01 22:52:30 -0400

    Implement MailRenderer. Renders the Velocity template for a given set of contents.

--- src/main/java/com/timostaudinger/dailydose/admin/MailRenderTestServlet.java
+++ src/main/java/com/timostaudinger/dailydose/admin/MailRenderTestServlet.java
@@ -1,8 +1,6 @@
 package com.timostaudinger.dailydose.admin;
 
-import org.apache.velocity.Template;
-import org.apache.velocity.VelocityContext;
-import org.apache.velocity.app.VelocityEngine;
+import com.timostaudinger.dailydose.render.MailRenderer;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,27 +8,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.StringWriter;
-import java.util.Properties;
 
-@WebServlet(name = "velocitytest", urlPatterns = {"/test/velocity"})
+@WebServlet(name = "mailrendertest", urlPatterns = {"/test/mailrender"})
 public class MailRenderTestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Properties props = new Properties();
-        props.setProperty("resource.loader", "class");
-        props.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
-
-        VelocityEngine velocityEngine = new VelocityEngine(props);
-
-        VelocityContext context = new VelocityContext();
-
-        velocityEngine.init();
-
-        Template template = velocityEngine.getTemplate("dailydose.vm");
-        StringWriter stringWriter = new StringWriter();
-        template.merge(context, stringWriter);
-        response.getWriter().print(stringWriter.toString());
+        String mail = new MailRenderer("Test", "http://i.imgur.com/WgYGR1z.jpg", "Bla", "Hello World").renderMail();
+        response.getWriter().print(mail);
     }
 }
--- src/main/java/com/timostaudinger/dailydose/render/MailRenderer.java
+++ src/main/java/com/timostaudinger/dailydose/render/MailRenderer.java
@@ -1,5 +1,64 @@
 package com.timostaudinger.dailydose.render;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
 public class MailRenderer {
 
+    private static final String IMAGE_TITLE = "imageTitle";
+    private static final String IMAGE_URL = "imageUrl";
+    private static final String QUOTE_TITLE = "quoteTitle";
+    private static final String QUOTE_CONTENT = "quoteContent";
+    private static final String CONTENT = "content";
+    private static final String TEMPLATE_NAME = "dailydose.vm";
+    private String imageTitle;
+    private String imageUrl;
+    private String quoteTitle;
+    private String quoteContent;
+
+    public MailRenderer(String imageTitle, String imageUrl, String quoteTitle, String quoteContent) {
+        this.imageTitle = imageTitle;
+        this.imageUrl = imageUrl;
+        this.quoteTitle = quoteTitle;
+        this.quoteContent = quoteContent;
+    }
+
+    public String renderMail() {
+
+        VelocityEngine velocityEngine = new VelocityEngine(getVelocityProperties());
+        velocityEngine.init();
+
+        Template template = velocityEngine.getTemplate(TEMPLATE_NAME);
+
+        StringWriter stringWriter = new StringWriter();
+        template.merge(mapFields(), stringWriter);
+
+        return stringWriter.toString();
+    }
+
+    private Properties getVelocityProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("resource.loader", "class");
+        properties.setProperty("class.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        return properties;
+    }
+
+    private VelocityContext mapFields() {
+        Map content = new HashMap();
+        content.put(IMAGE_TITLE, imageTitle);
+        content.put(IMAGE_URL, imageUrl);
+        content.put(QUOTE_TITLE, quoteTitle);
+        content.put(QUOTE_CONTENT, quoteContent);
+
+        VelocityContext context = new VelocityContext();
+        context.put(CONTENT, content);
+
+        return context;
+    }
 }
