102b59764fb565e419bfa63cb5944d509ef654ae
Timo Staudinger <staudinger.timo@gmail.com>
2015-06-06 18:39:20 -0400

    Add first unit tests.

---
+++ src/test/java/com/timostaudinger/dailydose/util/PropertiesTest.java
@@ -0,0 +1,33 @@
+package com.timostaudinger.dailydose.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class PropertiesTest {
+    @Test
+    public void testProperties() throws Exception {
+
+        String redditUsername = Properties.get("reddit_username");
+        assertNotNull(redditUsername);
+        assertTrue(redditUsername.length() > 0);
+
+        String redditAppId = Properties.get("reddit_app_id");
+        assertNotNull(redditAppId);
+        assertTrue(redditAppId.length() > 0);
+
+        String redditClientId = Properties.get("reddit_client_id");
+        assertNotNull(redditClientId);
+        assertTrue(redditClientId.length() > 0);
+
+        String redditClientSecret = Properties.get("reddit_client_secret");
+        assertNotNull(redditClientSecret);
+        assertTrue(redditClientSecret.length() > 0);
+
+        String reddit_platform = Properties.get("reddit_platform");
+        assertNotNull(reddit_platform);
+        assertTrue(reddit_platform.length() > 0);
+
+    }
+}
