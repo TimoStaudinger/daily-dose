27d90adb7eeae50215d4f1a8226caeecdddd497f
Timo Staudinger <staudinger.timo@gmail.com>
2015-06-16 18:56:40 -0400

    Consume find REST Endpoint.

--- dailydose.js
+++ dailydose.js
@@ -2,14 +2,52 @@ angular.module('dailydoseApp', [])
     .controller('DailydoseController', function ($scope, $http) {
         var dailydose = this;
 
+        dailydose.buttontext = "Sign me up!";
+        dailydose.subscribed = false;
+
         dailydose.subscribe = function () {
-            $http.get('http://localhost:8080/DailyDose/api/subscribe/' + dailydose.subscriber).
-                success(function (data, status, headers, config) {
-                    console.log(data);
-                }).
-                error(function (data, status, headers, config) {
-                    console.log("error");
-                });
+            if (!dailydose.subscribed) {
+                $http.get('http://localhost:8080/DailyDose/api/subscribe/' + dailydose.subscriber).
+                    success(function (data, status, headers, config) {
+                        console.log(data);
+                    }).
+                    error(function (data, status, headers, config) {
+                        console.log("error");
+                    });
+            } else {
+                $http.get('http://localhost:8080/DailyDose/api/unsubscribe/' + dailydose.subscriber).
+                    success(function (data, status, headers, config) {
+                        console.log(data);
+                    }).
+                    error(function (data, status, headers, config) {
+                        console.log("error");
+                    });
+            }
         };
+
+        dailydose.change = function () {
+            if (dailydose.subscriber != undefined && dailydose.subscriber.length > 0) {
+                dailydose.buttontext = "loading...";
+                $http.get('http://localhost:8080/DailyDose/api/find/' + dailydose.subscriber).
+                    success(function (data, status, headers, config) {
+                        if (data === "1") {
+                            dailydose.buttontext = "Unsubscribe";
+                            dailydose.subscribed = true;
+                        } else {
+                            dailydose.buttontext = "Sign me up!";
+                            dailydose.subscribed = false;
+
+                        }
+                    }).
+                    error(function (data, status, headers, config) {
+                        dailydose.buttontext = "Sign me up!";
+                        dailydose.subscribed = false;
+                    });
+                console.log("checking");
+            } else {
+                dailydose.buttontext = "Sign me up!";
+                dailydose.subscribed = false;
+            }
+        }
 
     });
